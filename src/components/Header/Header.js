e75fa2cd109a6fe9907a90d6bb9dad5e1ec9e6bc
thanhdkhe150032 <thanhdkhe150032@fpt.edu.vn>
2022-09-22 12:15:26 +0700

    C2: demo some important front-end syntax

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,8 +1,24 @@
 import React from 'react'
 import styles from './Header.module.css';
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import { NavLink } from "react-router-dom";
 
 export default function Header() {
     return (
-        <div>Header</div>
+        <div>Header
+            <Navbar bg="primary" variant="dark">
+        <Container>
+          <Navbar.Brand href="#home">Navbar</Navbar.Brand>
+          <Nav className="me-auto">
+            {/* How to navigate without button */}
+            <NavLink to="/login" activeStyle={{fontWeight: 'bold'}} className="text-white hover:text-lime-800 mr-2 no-underline">Login</NavLink>
+            <NavLink to="/logout" activeStyle={{fontWeight: 'bold'}} className="text-white hover:text-lime-800 mr-2 no-underline">Logout</NavLink>
+            <NavLink to="/register" activeStyle={{fontWeight: 'bold'}} className="text-white hover:text-lime-800 no-underline">Resister</NavLink>
+          </Nav>
+        </Container>
+      </Navbar>
+        </div>
     )
 }
--- src/pages/Homepage/Homepage.js
+++ src/pages/Homepage/Homepage.js
@@ -3,31 +3,46 @@ import styles from './Homepage.module.css';
 import { Pagination } from 'antd';
 import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
+import { history } from '../../App';
+import Header from '../../components/Header/Header';
+
 
 export default function Homepage() {
+    const handleNavigate = () => {
+        history.push("/login")
+    }
     return (
         //Demo cách sử dụng module.css trong react
-        <div>
-            <h1 className={styles.homepage_title}>Homepage</h1>
+        <div >
+            <Header/>
+        <h1 className = { styles.homepage_title } > Homepage </h1>
 
-            {/* Demo cách apply 1 UI trong ant design */}
-            <Pagination defaultCurrent={1} total={50} />
+        { /* Demo cách apply 1 UI trong ant design */ } 
+        <Pagination defaultCurrent = { 1 }
+        total = { 50 }
+        />
 
-            {/* Demo cách sử dụng tailwindcss */}
-            <div className='text-purple-400 hover:text-red-900 cursor-pointer'>Hello Thành</div>
+        { /* Demo cách sử dụng tailwindcss */ } 
+        <div className = 'text-purple-400 hover:text-red-900 cursor-pointer'> Hello Thành </div>
 
-            {/* Demo cách sử dụng thư viện react-bootstrap */}
-            <Card style={{ width: '18rem' }}>
-                <Card.Img variant="top" src="https://static.dw.com/image/44278679_303.jpg" />
-                <Card.Body>
-                    <Card.Title>Card Title</Card.Title>
-                    <Card.Text>
-                        Some quick example text to build on the card title and make up the
-                        bulk of the card's content.
-                    </Card.Text>
-                    <Button variant="primary">Go somewhere</Button>
-                </Card.Body>
-            </Card>
+        { /* Demo cách sử dụng thư viện react-bootstrap */ } 
+        <Card style = {
+            { width: '18rem' }
+        } >
+        <Card.Img variant = "top"
+        src = "https://w0.peakpx.com/wallpaper/954/841/HD-wallpaper-cristiano-ronaldo-epl-soccer-cr7-football-portugal-champion-manchester-united-red-devils-goat-premier-league-man-utd.jpg" />
+        <Card.Body>
+        <Card.Title> Card Title </Card.Title> 
+        <Card.Text>
+        Some quick example text to build on the card title and make up the bulk of the card 's content. </Card.Text > 
+        <Button variant = "primary" > Go somewhere </Button> 
+        {/* How to navigate to a page? */}
+        <button className='bg-lime-800' 
+        onClick={() => {handleNavigate()}}>
+            SIU
+            </button>
+        </Card.Body> 
+        </Card>
         </div>
     )
 }
