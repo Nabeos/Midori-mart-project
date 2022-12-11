d28971c9846521d5031f34965e310e0cb7281eff
Nguyen Khanh Toan <toannkhe150086@fpt.edu.vn>
2022-12-12 00:26:07 +0700

    change footer content

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -9,118 +9,36 @@ import { FaFacebook, FaEnvelope, FaPhone } from "react-icons/fa";
 export default function Footer() {
   return (
     <div>
-      <div>
-        <Navbar
-          bg="primary"
-          variant="dark"
-          className={`${styles.footer__background}`}
-        >
-          <Container>
-            {/* <Navbar.Brand href="#home">Navbar</Navbar.Brand> */}
-            <Nav className="">
-              {/* How to navigate without button */}
-              <div className=" ml-52 mb-2 text-md flex">
-                <div className=" flex -mr-24 mt-6">
-                  <NavLink
-                    to="/"
-                    activeStyle={{ fontWeight: "bold" }}
-                    className="text-black no-underline font-semibold ml-20"
-                  >
-                    <div className="flex items-center -ml-56">
-                      <img
-                        src="/images/midorimart_logo_nobg.png"
-                        style={{ width: "50%" }}
-                      />
-                    </div>
-                  </NavLink>
+      <Navbar
+        bg="primary"
+        variant="dark"
+        className={`${styles.footer__background}`}
+        style={{ height: "160px", width: "100%" }}
+      >
+        <Container>
+          <div className="text-md flex justify-between items-center" style={{ width: "100%" }}>
+            <div className="">
+              <NavLink
+                to="/"
+                activeStyle={{ fontWeight: "bold" }}
+                className="text-black no-underline font-semibold"
+              >
+                <div className="">
+                  <img
+                    src="/images/midorimart_logo_nobg.png"
+                    style={{ width: "50%" }}
+                  />
                 </div>
-                <div className="grid grid-cols-8 mt-2 mb-2 ml-24 text-md items-start">
-                  <div className="flex flex-col col-span-2 justify-center">
-                    <div className="text-white text-md font-medium">
-                      Về chúng tôi
-                    </div>
-                    <NavLink
-                      to="/logout"
-                      activeStyle={{ fontWeight: "bolder" }}
-                      className={`${styles.footer__hover} text-white no-underline text-md font-normal mt-3 `}
-                    >
-                      Giới thiệu Midori Mart
-                    </NavLink>
-                    <NavLink
-                      to="/logout"
-                      activeStyle={{ fontWeight: "bolder" }}
-                      className={`${styles.footer__hover} text-white no-underline text-md font-normal`}
-                    >
-                      Hệ thống cửa hàng
-                    </NavLink>
-                  </div>
-                  <div className="flex flex-col col-span-3 justify-start ml-8">
-                    <div className="text-white text-md font-medium">
-                      Hỗ trợ khách hàng
-                    </div>
-                    <NavLink
-                      to="/logout"
-                      activeStyle={{ fontWeight: "bolder" }}
-                      className={`${styles.footer__hover} text-white no-underline text-md font-normal mt-3 `}
-                    >
-                      Hướng dẫn mua hàng
-                    </NavLink>
-                    <NavLink
-                      to="/logout"
-                      activeStyle={{ fontWeight: "bolder" }}
-                      className={`${styles.footer__hover} text-white no-underline text-md font-normal`}
-                    >
-                      Chính sách giao hàng
-                    </NavLink>
-                    <NavLink
-                      to="/logout"
-                      activeStyle={{ fontWeight: "bolder" }}
-                      className={`${styles.footer__hover} text-white no-underline text-md font-normal`}
-                    >
-                      Chính sách đổi trả
-                    </NavLink>
-                    <NavLink
-                      to="/logout"
-                      activeStyle={{ fontWeight: "bolder" }}
-                      className={`${styles.footer__hover} text-white no-underline text-md font-normal`}
-                    >
-                      Chính sách thanh toán
-                    </NavLink>
-                  </div>
-                  <div className="flex flex-col col-span-3 justify-start ">
-                    <div className="text-white text-md font-medium">
-                      Chăm sóc khách hàng
-                    </div>
-                    <div className="flex flex-row mt-3">
-                      <NavLink
-                        to="/logout"
-                        activeStyle={{ fontWeight: "bolder" }}
-                        className={`${styles.facebook__hover} text-white no-underline`}
-                      >
-                        <FaFacebook className="text-2xl" />
-                      </NavLink>
-                      <NavLink
-                        to="/logout"
-                        activeStyle={{ fontWeight: "bolder" }}
-                        className={`${styles.mail__hover} text-white ml-4 no-underline`}
-                      >
-                        <FaEnvelope className="text-2xl" />
-                      </NavLink>
-                      <NavLink
-                        to="/logout"
-                        activeStyle={{ fontWeight: "bolder" }}
-                        className={`${styles.phone__hover} text-white ml-4 no-underline`}
-                      >
-                        <FaPhone className="text-2xl" />
-                      </NavLink>
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </Nav>
-          </Container>
-        </Navbar>
-      </div>
+              </NavLink>
+            </div>
+            <div className="">
+              <p className="text-white text-lg">
+                Chúc quý khách có trải nghiệm mua sắm tuyệt vời tại Midori Mart
+              </p>
+            </div>
+          </div>
+        </Container>
+      </Navbar>
     </div>
   );
 }
